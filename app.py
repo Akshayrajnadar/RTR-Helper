ce2f39aac2ccdadda7279d7fedd84b61e1bf304d
Akshayraj Nadar <akshayraj08032000@gmail.com>
2025-01-23 23:18:21 +0530

    imported required liabries

---
+++ app.py
@@ -0,0 +1,5 @@
+from langchian_community.document_loaders import PyPDFLoader
+from langchian_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchian_community.vectorstores import FAISS
+

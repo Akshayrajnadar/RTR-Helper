af02953cc0be24e9a75d66c0d0dd8cb1564cf05f
Akshayraj Nadar <akshayraj08032000@gmail.com>
2025-01-24 13:29:12 +0530

    Created ingest file

--- app.py
+++
@@ -1,5 +0,0 @@
-from langchian_community.document_loaders import PyPDFLoader
-from langchian_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchian_community.vectorstores import FAISS
-
